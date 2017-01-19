e4f8d64f8ea91a11d489683df823959560cdb6dd
NICK VALLEE <nickvallee@gmail.com>
2017-01-19 14:57:20 -0500

    commiting template files

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,38 @@
+
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+gulp.task('watch', function() {
+
+    /* this is creating a local server */
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "app"
+        }
+    });
+
+
+/*watch for saved changes to html file */
+    watch('./app/index.html', function() {
+       browserSync.reload();
+    });
+
+    watch('./app/assets/styles/**/*.css', function() {
+        gulp.start('cssInject');
+    });
+
+    watch('./app/assets/scripts/**/*.js', function() {
+        gulp.start('scriptsRefresh');
+    });
+});
+
+gulp.task('cssInject',['styles'], function() {
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh',['scripts'], function() {
+    browserSync.reload()
+});
